baf7011e7d04f16700ac948da9603880d47b6506
StarNeit <koneits@outlook.com>
2018-10-19 19:25:44 +0200

    Prepare block generation

--- src/miner.cpp
+++ src/miner.cpp
@@ -494,13 +494,13 @@ bool ProcessBlockFound(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
     }
 
     // Process this block the same as if we had received it from another node
-    CValidationState state;
-    if (!ProcessNewBlock(state, NULL, pblock))
-        return error("PIVXMiner : ProcessNewBlock, block not accepted");
-
-    for (CNode* node : vNodes) {
-        node->PushInventory(CInv(MSG_BLOCK, pblock->GetHash()));
-    }
+//    CValidationState state;
+//    if (!ProcessNewBlock(state, NULL, pblock))
+//        return error("PIVXMiner : ProcessNewBlock, block not accepted");
+//
+//    for (CNode* node : vNodes) {
+//        node->PushInventory(CInv(MSG_BLOCK, pblock->GetHash()));
+//    }
 
     return true;
 }
