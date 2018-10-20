a67d3488753165aa2e1294b2c6dad6f56a319bd6
StarNeit <koneits@outlook.com>
2018-10-20 16:49:51 +0300

    Update copyright

--- src/libzerocoin/Coin.cpp
+++ src/libzerocoin/Coin.cpp
@@ -10,7 +10,7 @@
  * @license    This project is released under the MIT license.
  **/
 
-// Copyright (c) 2017 The DWE developers
+// Copyright (c) 2018 The DWE developers
 #include <stdexcept>
 #include <iostream>
 #include "Coin.h"
